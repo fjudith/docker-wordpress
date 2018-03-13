46d07976c45c4a0890b1b6866ea4a2f72f1c987e
Florian JUDITH <florian.judith.b@gmail.com>
2018-03-13 10:28:46 +0100

    fixed deployment api version

--- kubernetes/create.sh
+++ kubernetes/create.sh
@@ -1,5 +1,5 @@
 tr --delete '\n' <password.txt >.strippedpassword.txt && mv .strippedpassword.txt password.txt
-kubectl create -f https://raw.githubusercontent.com/fjudith/docker-wordpress/master/kubernetes/mysql-wordpress-pd/local-volumes.yaml
+kubectl create -f https://raw.githubusercontent.com/fjudith/docker-wordpress/master/kubernetes/local-volumes.yaml
 kubectl create secret generic wp-mysql-pass --from-file=password.txt
-kubectl create -f https://raw.githubusercontent.com/fjudith/docker-wordpress/master/kubernetes/mysql-wordpress-pd/mysql-deployment.yaml
-kubectl create -f https://raw.githubusercontent.com/fjudith/docker-wordpress/master/kubernetes/mysql-wordpress-pd/wordpress-deployment.yaml
+kubectl create -f https://raw.githubusercontent.com/fjudith/docker-wordpress/master/kubernetes/mysql-deployment.yaml
+kubectl create -f https://raw.githubusercontent.com/fjudith/docker-wordpress/master/kubernetes/wordpress-deployment.yaml
