b0bb798c33a4b3dabfeeab506988392be39448f3
Florian JUDITH <florian.judith.b@gmail.com>
2018-03-26 18:19:42 +0200

    Separate ingress YAML to avoid conflict with Conduit

--- kubernetes/k8swordpress.sh
+++ kubernetes/k8swordpress.sh
@@ -23,8 +23,8 @@ elif [ -v create ] && [ "$create" == "conduit" ]; then
   tr --delete '\n' <password.txt >.strippedpassword.txt && mv .strippedpassword.txt password.txt
   kubectl apply -f ./local-volumes.yaml
   kubectl create secret generic wp-mysql-pass --from-file=password.txt
-  curl ./mysql-deployment.yaml | conduit inject --skip-inbound-ports=3306 - | kubectl apply -f -
-  curl ./wordpress-deployment.yaml | conduit inject --skip-outbound-ports=3306,9000,11211 --skip-inbound-ports=3306,9000,11211 - | kubectl apply -f -
+  cat ./mysql-deployment.yaml | conduit inject --skip-inbound-ports=3306 - | kubectl apply -f -
+  cat ./wordpress-deployment.yaml | conduit inject --skip-outbound-ports=3306,9000,11211 --skip-inbound-ports=3306,9000,11211 - | kubectl apply -f -
 
   kubectl get svc nginx -n default -o jsonpath="{.status.loadBalancer.ingress[0].*}"
 
@@ -38,6 +38,7 @@ elif [ -v create ] && [ "$create" == "istio" ]; then
   kubectl create secret -n wordpress  generic wp-mysql-pass --from-file=password.txt
   kubectl apply -n wordpress -f ./mysql-deployment.yaml
   kubectl apply -n wordpress -f ./wordpress-deployment.yaml
+  kubectl apply -n wordpress -f ./wordpress-ingress.yaml
 
   export GATEWAY_URL=$(kubectl get po -l istio=ingress -n istio-system -o 'jsonpath={.items[0].status.hostIP}'):$(kubectl get svc istio-ingress -n istio-system -o 'jsonpath={.spec.ports[0].nodePort}')
 
@@ -58,6 +59,7 @@ if [ -v delete ] && [ "$delete" == "istio" ]; then
   kubectl delete secret wp-mysql-pass
   kubectl delete -n wordpress -f ./mysql-deployment.yaml
   kubectl delete -n wordpress -f ./wordpress-deployment.yaml
+  kubectl delete -n wordpress -f ./wordpress-ingress.yaml
 
   kubectl delete namespace wordpress
 fi
